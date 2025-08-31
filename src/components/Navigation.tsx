7f5d40d4133d2485bb407573ee8fa793c2774b31
ViniciusAlmeida <vinicius.quadros@icloud.com>
2025-08-31 16:42:33 -0300

    Corrige carregamento do logo com fallback e tratamento de erro

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -13,10 +13,16 @@ const Navigation: React.FC = () => {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between h-16">
           <div className="flex items-center">
-            <img
-              src="/3IAN-ORANGE.png"
-              alt="Logo"
+            <img 
+              src="/3IAN-ORANGE.png" 
+              alt="Logo" 
               className="h-10 w-auto"
+              onError={(e) => {
+                console.error("Erro ao carregar logo:", e);
+                // Tentar caminho alternativo
+                const target = e.target as HTMLImageElement;
+                target.src = process.env.PUBLIC_URL + "/3IAN-ORANGE.png";
+              }}
             />
           </div>
 
